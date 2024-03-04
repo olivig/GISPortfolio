d967bfca870d1930050c2ec2f819def4fa96e203
olivig <159450425+olivig@users.noreply.github.com>
2024-03-04 21:46:00 +0100

    adding new functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
-  })
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"});
+  });
 }
+
+// JavaScript for the Modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("gallery-image");
+var modalImg = document.getElementById("modalImg");
+
+for (var i = 0; i < img.length; i++) {
+  img[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
